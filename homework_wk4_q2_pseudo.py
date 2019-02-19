47c93a24e0e5fbf1b36c32543a955a7dd2fc2bcb
Jonathan Rios <47791907+RiosJon@users.noreply.github.com>
2019-02-19 12:58:52 -0700

    Update homework_wk4_q2_pseudo.py

--- homework_wk4_q2_pseudo.py
+++ homework_wk4_q2_pseudo.py
@@ -9,18 +9,18 @@
 # outputs will be times + which comes first
 # ********************************************
 
-hour1 = int(input('---figure out an input-----')) 
-hour2 = int(input('Give me one more')) 
-minute1 = int(input('give minutes')) 
-minute2 = int(input('give minutes'))
-if hour1 < hour2: 
-    print(hour1 + 'comes before' + hour2)
+hour1 = int(input('Give me hours in military time' ))
+hour2 = int(input('Give me one more' ))
+minute1 = int(input('give minutes for first time' ))
+minute2 = int(input('give minutes for second time' ))
+if hour1 < hour2:
+    print(print(hour1, 'comes before', hour2)
 elif hour1 > hour2:
-    print(hour2 + 'comes before' + hour1 )
-else: 
+    print(hour2, 'comes before', hour1)
+else:
     if minute1 < minute2:
-        print(hour1 + 'comes before' + hour2)
+        print(hour1, 'comes before', hour2)
     elif minute1 > minute2:
-        print(hour2 + "comes before" + hour1) 
+        print(hour2, "comes before", hour1)
     else:
         print('both times are the same')
