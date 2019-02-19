191ff4f1a1ebc65a95a210853fc6369269212f23
Jonathan Rios <47791907+RiosJon@users.noreply.github.com>
2019-02-19 12:28:05 -0700

    Update homework_wk4_q2_pseudo.py

--- homework_wk4_q2_pseudo.py
+++ homework_wk4_q2_pseudo.py
@@ -9,22 +9,18 @@
 # outputs will be times + which comes first
 # ********************************************
 
-# WL: You were missing a ")" at the end of the input() for both this:
-hour1 = input(int('---figure out an input-----'))
-# WL: ...and this one, too.
-hour2 = input(int('Give me one more'))
-# might have to possibly input minute inputs here
-# minute1 = input(int('')
-# minute2 = input(int('')
-
-if hour1 < hour2:
-    # WL: hour1 comes before hour2
+hour1 = int(input('---figure out an input-----')) 
+hour2 = int(input('Give me one more')) 
+minute1 = int(input('give minutes')) 
+minute2 = int(input('give minutes'))
+if hour1 < hour2: 
+    print(hour1 + 'comes before' + hour2)
 elif hour1 > hour2:
-    # WL: hour2 comes before hour1
-else:
+    print(hour2 + 'comes before' + hour1 )
+else: 
     if minute1 < minute2:
-        # WL: hour1 comes before hour2
+        print(hour1 + 'comes before' + hour2)
     elif minute1 > minute2:
-        # WL: hour2 comes before hour1
+        print(hour2 + "comes before" + hour1) 
     else:
-        # WL: both times are the same
+        print('both times are the same')
