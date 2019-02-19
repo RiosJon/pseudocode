de8f2f72b7be5d5b68324f59ad66991449f1e9c0
Jonathan Rios <47791907+RiosJon@users.noreply.github.com>
2019-02-19 11:44:56 -0700

    Update homework wk4 q2.py pseudo.py

--- homework wk4 q2.py pseudo.py
+++ homework wk4 q2.py pseudo.py
@@ -11,6 +11,8 @@
 hour1 = input(int('---figure out an input-----')
 hour2 = input(int('Give me one more')
 #might have to possibly input minute inputs here
+#minute1 = input(int('')
+#minute2 = input(int('')
 
 if hour1 < hour2:
               time1 comes first
