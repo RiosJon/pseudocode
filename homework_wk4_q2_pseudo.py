68d691b34364e4a4afce5b171ac9bc18ca7e0b2b
Jonathan Rios <47791907+RiosJon@users.noreply.github.com>
2019-02-19 14:25:25 -0700

    Update homework_wk4_q2_pseudo.py

--- homework_wk4_q2_pseudo.py
+++ homework_wk4_q2_pseudo.py
@@ -9,18 +9,23 @@
 # outputs will be times + which comes first
 # ********************************************
 
-hour1 = int(input('Give me hours in military time' ))
-hour2 = int(input('Give me one more' ))
-minute1 = int(input('give minutes for first time' ))
-minute2 = int(input('give minutes for second time' ))
+hour1 = int(input('Give me hours only in military time ' ))
+minute1 = int(input('give minutes for first time ' ))
+full_hour1 = str(hour1) + str(minute1)
+hour2 = int(input('Give me hours only in military time for the 2nd one ' ))
+minute2 = int(input('give me minutes for the second hour ' ))
+full_hour2 = str(hour2) + str(minute2)
 if hour1 < hour2:
-    print(print(hour1, 'comes before', hour2)
+    print(full_hour1, 'comes before', full_hour2)
 elif hour1 > hour2:
-    print(hour2, 'comes before', hour1)
+    print(full_hour1 ,"comes before", full_hour2)
+elif hour1 >23 or hour2 >23:
+    print("That's not a correct hour in military time")
 else:
     if minute1 < minute2:
-        print(hour1, 'comes before', hour2)
+        print(full_hour1, 'comes before', full_hour2)
     elif minute1 > minute2:
-        print(hour2, "comes before", hour1)
+        print(full_hour2, "comes before", full_hour1)
     else:
         print('both times are the same')
+
