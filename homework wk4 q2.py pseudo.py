14ae73752a97639920d60f2c858e2e39bf081199
Jonathan Rios <47791907+RiosJon@users.noreply.github.com>
2019-02-19 11:33:42 -0700

    Add files via upload

---
+++ homework wk4 q2.py pseudo.py
@@ -0,0 +1,29 @@
+#*******************************************************
+#Program Name: 3.18 pg 153
+#Programmer: Jonathan Rios
+#CSC-119     Spring 2019
+#Date:02/18/2019
+#purpose: FOllow the pseudo code
+    #when two points in time are compared each given as hours (in military time, ranging from 0-23)
+    #and minutes, the following pseudocode determines which comes first
+#write a program that prompts the user for 2 points in time and prints the time that comes first, then the other time
+#********************************************
+#inputs will be times
+#outputs will be times + which comes first
+#********************************************
+
+hour1 = input(int('---figure out an input-----')
+hour2 = input(int('Give me one more')
+#might have to possibly input minute inputs here
+
+if hour1 < hour2:
+              time1 comes first
+else if hour1 and hour2 are the same:
+              if minute1 < minute2
+              time1 comes first
+              else if minute1 and minute2 are the same
+              time 1 and time2 are the same
+              else
+              time2 comes first
+              
+              
